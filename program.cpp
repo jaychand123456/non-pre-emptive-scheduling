74c98ec7ccd0aeda9cc34831f3d32becebee7354
Rohit Lalwani <lalwanirohit71@gmail.com>
2020-03-24 17:55:35 +0530

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -29,4 +29,74 @@ int main() {
 		temp.turnaround = 0;
 		maps[at].push_back(temp);
 	}
+	map<int, vector<node> >::iterator itr;
+	//running queue
+	vector<node> running;
+	//done queue
+	vector<node> done;
+	for(int i=0;i<maps.begin()->second.size();++i) {
+		running.push_back(maps.begin()->second[i]);
+	}
+	maps.erase(maps.begin());
+// 	sorting running list
+	for(int i=0;i<running.size();++i) {
+		for(int j=0;j<running.size()-i-1;++j) {
+			if(running[j].burst>running[j+1].burst) {
+				node temp = running[j];
+				running[j] = running[j+1];
+				running[j+1] = temp;
+			}
+		}
+	}
+	for(int i=0;i<running.size();++i) {
+		running[i].priority = 1;
+	}
+	int timer = running[0].arrival;
+	while(done.size()!=n) {
+		timer += running[0].burst;
+		running[0].turnaround = timer - running[0].arrival;
+		done.push_back(running[0]);
+		running.erase(running.begin());
+		for(itr=maps.begin();itr!=maps.end();++itr) {
+			if(itr->first<=timer) {
+				for(int i=0;i<itr->second.size();++i) {
+					bool inrunning = false;
+					for(int j=0;j<running.size();++j) {
+						if(running[j].process==itr->second[i].process) {
+							inrunning = true;
+							break;
+						}
+					}
+					bool indone = false;
+					for(int j=0;j<done.size();++j) {
+						if(done[j].process==itr->second[i].process) {
+							indone = true;
+							break;
+						}
+					}
+					if(inrunning==false && indone==false) {
+						running.push_back(itr->second[i]);
+					}
+				}
+			}
+		}
+		for(int i=0;i<running.size();++i) {
+				running[i].waiting = timer - running[i].arrival;
+				running[i].priority = 1 + ((running[i].waiting)/running[i].burst);
+		}
+		for(int i=0;i<running.size();++i) {
+			for(int j=0;j<running.size()-i-1;++j) {
+				if(running[j].priority<running[j+1].priority) {
+					node temp = running[j];
+					running[j] = running[j+1];
+					running[j+1] = temp;
+				}
+			}
+		}
+	}
+	printf(" * * * * * After scheduling * * * * *\n");
+	for(int i=0;i<n;++i) {
+		printf("P%d\t%d\t%d\n", done[i].process, done[i].waiting, done[i].turnaround);
+	}
+	return 0;
 }
